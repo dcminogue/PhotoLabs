15e2d3d4c3ec986b99d9ad41dda53607bbbeb840
dcminogue <danielcminogue@gmail.com>
2024-06-02 21:46:22 -0600

    Add a TopicListItem component to list out different topics available. Add a TopicList component to group TopicListItem components into a single component.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-
+import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
 const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
+    {
+        id: "1",
+        slug: "topic-1",
+        title: "Nature",
+    },
+    {
+        id: "2",
+        slug: "topic-2",
+        title: "Travel",
+    },
+    {
+        id: "3",
+        slug: "topic-3",
+        title: "People",
+    },
 ];
 
 const TopicList = () => {
-  return (
-    <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
-    </div>
-  );
+    return (
+        <div className="top-nav-bar__topic-list">
+            {sampleDataForTopicList.map(topic => (
+                <TopicListItem key={topic.id} topic={topic} />
+            ))}
+        </div>
+    );
 };
 
 export default TopicList;
