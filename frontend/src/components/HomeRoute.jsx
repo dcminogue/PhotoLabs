65470a55930de18ccc28172d9f7c839054085d0b
dcminogue <danielcminogue@gmail.com>
2024-06-03 10:33:14 -0600

    Add a HomeRoute.jsx component and inside of the HomeRoute component place the TopNavigationBar and PhotoList components. Add HomeRoute component to App.jsx.

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import TopNavigationBar from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
 
-const HomeRoute = () => (
-    <div className="HomeRoute">
-        <TopNavigationBar />
-        <PhotoList />
-    </div>
-);
+const HomeRoute = () => {
+    return (
+        <div className="HomeRoute">
+            <TopNavigationBar />
+            <PhotoList />
+        </div>
+    );
+};
 
 export default HomeRoute;
